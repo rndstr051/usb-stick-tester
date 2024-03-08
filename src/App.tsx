c4f031d1e34699716347caaf2b80a2fc06bb1e75
rndstr051 <20374809+rndstr051@users.noreply.github.com>
2024-03-08 16:48:14 +0000

    suid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import type { Component } from 'solid-js';
 
 import styles from './App.module.css';
+import { Button } from '@suid/material';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      
+      <Button variant='contained'>asd</Button>
     </div>
   );
 };
