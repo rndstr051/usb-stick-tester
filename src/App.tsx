aac090fe5797306b4b4b6a48706ff001966fec41
rndstr051 <20374809+rndstr051@users.noreply.github.com>
2024-03-08 17:49:44 +0000

    Step

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,44 @@ import { createSignal, type Component } from 'solid-js';
 import styles from './App.module.css';
 import { Button } from '@suid/material';
 
+const calculateGraph = (timestamps: number[]) => {
+  timestamps.slice(10);
+
+  const min = Math.min(...timestamps);
+  const max = Math.max(...timestamps);
+  const speeds: number[] = [];
+  const points: number[] = [];
+
+  const step = 4;
+
+  for (let i = 0; i < timestamps.length - step; i += step) {
+    if (timestamps[i + step] !== timestamps[i]) {
+      speeds.push(1 / (timestamps[i + step] - timestamps[i]));
+    }
+    else {
+      speeds.push(0);
+    }
+  }
+  // const minSpeed = Math.min(...timestamps);
+  const maxSpeed = Math.max(...speeds);
+
+  for (let i = 0; i < timestamps.length - step; i += step) {
+    points.push((timestamps[i] - min) / (max - min) * 1000);
+    points.push(300 - speeds[i / step] / maxSpeed * 300);
+    // points.push(20);
+
+    points.push((timestamps[i + step] - min) / (max - min) * 1000);
+    points.push(300 - speeds[i / step] / maxSpeed * 300);
+    // points.push(50);
+  }
+
+  return points;
+}
+
 const App: Component = () => {
   const [deviceFolder, setDeviceFolder] = createSignal<FileSystemDirectoryHandle | undefined>()
   const [running, setRunning] = createSignal(false);
-
+  const [timestamps, setTimestamps] = createSignal<number[]>([])
 
   return (
     <div class={styles.App}>
@@ -26,12 +60,21 @@ const App: Component = () => {
           const folder = await deviceFolder()?.getDirectoryHandle("usb-stick-tester", { create: true });
           console.log(folder)
 
-          let currentBuffer = new Uint8Array(128 * 1024 * 1024);
-          let backBuffer = new Uint8Array(128 * 1024 * 1024);
-          let closePromise: Promise<void> | undefined;
+          let buffers: Uint8Array[] = new Array(4);
+          let closePromise: (Promise<void> | undefined)[] = new Array(4);
+
+          for (let i = 0; i < buffers.length; i += 1) {
+            buffers[i] = new Uint8Array(128 * 1024 * 1024);
+          }
+
+          setTimestamps([performance.now()]);
 
           try {
             while (running()) {
+              await closePromise[0];
+              setTimestamps(o => [...o, performance.now()]);
+
+              const currentBuffer = buffers[0];
               for (let i = 0; i < currentBuffer.length; i += 65536) {
                 crypto.getRandomValues(currentBuffer.subarray(i, i + 65536));
               }
@@ -39,13 +82,17 @@ const App: Component = () => {
               const hashBuffer = await crypto.subtle.digest('SHA-256', currentBuffer);
               const hashStr = [...new Uint8Array(hashBuffer)].map(o => o.toString(16).padStart(2, "0")).join("");
 
-              await closePromise;
               const file = await folder?.getFileHandle(`${hashStr}.dat`, { create: true });
               const stream = await file!.createWritable({ keepExistingData: false });
               await stream.write(currentBuffer);
-              closePromise = stream.close();
 
-              [currentBuffer, backBuffer] = [backBuffer, currentBuffer];
+              closePromise[0] = stream.close();
+
+              buffers.push(buffers.shift()!);
+              closePromise.push(closePromise.shift());
+
+              // [currentBuffer, backBuffer] = [backBuffer, currentBuffer];
+
             }
           }
           catch (ex) {
@@ -61,6 +108,10 @@ const App: Component = () => {
           setRunning(false)
         }
       }>Abort</Button>
+
+      <svg width={1000} height={300} style={{ background: "#f0f0f0" }}>
+        <polyline points={calculateGraph(timestamps()).join(',')} fill="none" stroke="black"></polyline>
+      </svg>
     </div>
   );
 };
